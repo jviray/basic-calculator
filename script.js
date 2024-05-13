f719b9e7025c1a26d1df4e09d4149d39deda1e19
Jonathan Viray <jfviray7@gmail.com>
2024-05-12 23:53:16 -0500

    Implement main operations

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+/**
+ * TODO:
+ * - Handle more than one input (operator pressed will also calculate)
+ * - Handle multiple pushes of operator OR equals
+ * - Allow key press inputs (not just clicks)
+ * - Limit input
+ * - Hover states
+ */
+
 const filterElementsByClass = (elements) => (className) => {
   // Converting NodeList to Array (Alternative: [...elements])
   return Array.from(elements).filter((element) =>
@@ -5,13 +14,91 @@ const filterElementsByClass = (elements) => (className) => {
   );
 };
 
-const resultDisplay = document.querySelector('input');
-const buttons = document.querySelectorAll('button');
-const searchButtonsFor = filterElementsByClass(buttons);
-const numbers = searchButtonsFor('number');
-
-for (const number of numbers) {
-  number.addEventListener('click', (event) => {
-    resultDisplay.value = number.value;
+// Must use normal function because of `this`
+function onClick(fn) {
+  this.addEventListener('click', (event) => {
+    fn(event);
   });
 }
+
+const calculate = () => {
+  const symbols = {
+    add: '+',
+    subtract: '-',
+    multiply: '*',
+    divide: '/',
+  };
+
+  const performOperation = new Function(
+    `return ${firstValue} ${symbols[operation]} ${secondValue}`
+  );
+  firstValue = performOperation();
+  display.value = firstValue;
+};
+
+const clearAll = () => {
+  firstValue = '';
+  secondValue = '';
+  operation = '';
+  display.value = '0';
+};
+
+// ======================================================
+
+// Elements
+const display = document.querySelector('input');
+const numbers = document.querySelectorAll('.number');
+const decimal = document.querySelector('#decimal');
+const operators = document.querySelectorAll('.operator');
+const equals = document.querySelector('#equals');
+const clear = document.querySelector('#clear');
+
+// App state
+let firstValue = '';
+let secondValue = '';
+let operation;
+
+// Handle numbers (Trick: when to move onto second number)
+for (const number of [...numbers]) {
+  onClick.bind(number)((e) => {
+    const value = e.target.value;
+    if (!operation) {
+      firstValue =
+        !firstValue || firstValue === '0' ? value : firstValue + value;
+      display.value = firstValue; // update
+    } else {
+      secondValue =
+        !secondValue || secondValue === '0' ? value : secondValue + value;
+      display.value = secondValue; // update
+    }
+  });
+}
+
+// Handle decimals (Trick: 0 and multiple decimals)
+onClick.bind(decimal)((e) => {
+  if (!operation && !firstValue.includes('.')) {
+    firstValue = !firstValue ? '0.' : firstValue + '.';
+    display.value = firstValue; // update
+  } else if (operation && !secondValue.includes('.')) {
+    secondValue = !secondValue ? '0.' : secondValue + '.';
+    display.value = secondValue; // update
+  }
+});
+
+// Handle operators
+for (const operator of operators) {
+  onClick.bind(operator)((e) => {
+    operation = e.target.id;
+  });
+}
+
+// Handle equals
+onClick.bind(equals)(calculate);
+
+// Handle clear
+onClick.bind(clear)(clearAll);
+
+/**
+ * - Equal won't do anything
+ * - Operators same as equal
+ */
